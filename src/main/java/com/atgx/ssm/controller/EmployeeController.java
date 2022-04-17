a427dcebc3b30fb62283e9db1f836615a301e8ee
liugx <sunshine_lgx@163.com>
2022-04-18 02:09:31 +0800

    xiug

--- src/main/java/com/atgx/ssm/controller/EmployeeController.java
+++ src/main/java/com/atgx/ssm/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.atgx.ssm.controller;
 
+import com.atgx.ssm.bean.EMP;
 import com.atgx.ssm.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class EmployeeController {
@@ -12,7 +15,7 @@ public class EmployeeController {
     EmployeeService employeeService;
 
     @RequestMapping("/aa")
-    public void getEmp(){
-        employeeService.queryAllUser();
+    public List<EMP> getEmp() {
+        return employeeService.queryAllUser();
     }
 }
