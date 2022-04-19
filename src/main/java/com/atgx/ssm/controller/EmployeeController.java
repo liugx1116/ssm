e1035000d518d00b4aa6b07e424d1781662bd711
liugx <sunshine_lgx@163.com>
2022-04-20 00:04:25 +0800

    查询所有用户-json

--- src/main/java/com/atgx/ssm/controller/EmployeeController.java
+++ src/main/java/com/atgx/ssm/controller/EmployeeController.java
@@ -4,9 +4,12 @@ import com.atgx.ssm.bean.EMP;
 import com.atgx.ssm.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -15,8 +18,11 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
-    @RequestMapping("/aa")
-    public List<EMP> getEmp() {
-        return employeeService.queryAllUser();
+    @RequestMapping("/getEmp")
+    @ResponseBody
+    public List<EMP> getEmp(HttpServletRequest request) {
+        List<EMP> emps = employeeService.queryAllUser();
+
+        return emps;
     }
 }
--- src/main/java/com/atgx/ssm/service/EmployeeService.java
+++ src/main/java/com/atgx/ssm/service/EmployeeService.java
@@ -5,6 +5,7 @@ import com.atgx.ssm.dao.EMPMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
