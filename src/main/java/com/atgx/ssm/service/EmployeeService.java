e2a08ea1a345c4acc0da7a0323781c4d0f49dfd7
liugx <sunshine_lgx@163.com>
2022-04-19 16:28:47 +0800

    ssm-crud 框架基础搭建

--- src/main/java/com/atgx/ssm/service/EmployeeService.java
+++ src/main/java/com/atgx/ssm/service/EmployeeService.java
@@ -2,8 +2,10 @@ package com.atgx.ssm.service;
 
 import com.atgx.ssm.bean.EMP;
 import com.atgx.ssm.dao.EMPMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -11,6 +13,7 @@ import java.util.List;
 @Transactional
 public class EmployeeService {
 
+    @Autowired
     EMPMapper empMapper;
     public List<EMP> queryAllUser() {
         List<EMP> emps = empMapper.selectByExample(null);
